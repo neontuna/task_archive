248718744ea5bbf708fae98d214dfe8fdf34f617
Justin Mullis <justin.mullis@gmail.com>
2016-08-23 12:50:44 -0400

    touchup search results, trim preview

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,56 +1,36 @@
 class SearchController < ApplicationController
+  
   def search
-    if !params[:q] || params[:q].length < 1
+    @search_term = params[:q] # used in view
+    @search_type = params[:type]    
+
+    if !@search_term || @search_term.length < 1
       flash[:warning] = 
         "Please enter a longer search term"
       redirect_to_back
-    else
-      @search_term = params[:q]
-      @search_type = params[:type]    
-      # defaults to searching customers, if type param present, check for contact
-      # of task
-
+    else  
+      # Defaults to task search, if type param is set will search contacts
+      # or customers
       if @search_type == "contacts"
-        @contacts = Contact.search(params[:q]).paginate(page: params[:page])
-      elsif @search_type == "tasks"
-        @tasks = Task.search(params[:q])
-        @workentries = Workentry.search(params[:q])
+        @contacts = Contact.search(@search_term).paginate(page: params[:page])
+      elsif @search_type == "customers"
+        @customers = Customer.search(@search_term).paginate(page: params[:page])
+      else
+        @tasks = Task.search(@search_term)
+        @workentries = Workentry.search(@search_term)
         @tasks_and_entries_all = @tasks + @workentries
 
-        # custom pagination junk
+        # custom pagination needed due to heterogeneous tasks and entries collection
         current_page = (params[:page] || 1).to_i
         per_page = 20
-        @tasks_and_entries = WillPaginate::Collection.create(current_page, 30, @tasks_and_entries_all.length) do |pager|
+        @tasks_and_entries = WillPaginate::Collection.create(current_page, per_page, @tasks_and_entries_all.length) do |pager|
           start = (current_page-1) * per_page # assuming current_page is 1 based.
           pager.replace(@tasks_and_entries_all[start, per_page])
         end
-      else
-        @customers = Customer.search(params[:q]).paginate(page: params[:page])
       end
 
-      # TODO - figure out task searching on title, description AND workentry summary_notes and internal_notes.  
-
       # TODO - figure out searching tasks within contacts and customer
-
-      # @snap_its = SnapIt
-      #   .search(params[:q])
-      #   .paginate(page: params[:page], per_page: 20)
-
-      # users = User.search(params[:q])
-      # profiles = Profile.search(params[:q])
-      # @users = User
-      #   .where( id: (users.pluck(:id) + profiles.pluck(:user_id) ) )
-      #   .paginate(page: params[:page], per_page: 20)
     end
   end
 
-  private
-
-  def get_type
-    if params[:type] == "contact"
-      return :customer
-    elsif params[:type] == "task"
-      return :task
-    end
-  end
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,19 @@
 module SearchHelper
+  def trim_and_highlight(text, search_term)
+    preview_chars = 300
+    search_term = sanitize(search_term)
+
+    if index = text.index(search_term)
+      trim = search_term.length + preview_chars
+      start = index - trim >= 0 ? index - trim : 0
+      if start <= 3
+        result = "#{text[start..(index + trim)]}..."
+      else
+        result = "...#{text[start..(index + trim)]}..."
+      end
+    else
+      result = "#{text[0..preview_chars]}..."
+    end
+    highlight(simple_format(result.html_safe), search_term)
+  end
 end
