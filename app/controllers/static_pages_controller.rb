e50f00089494de24741f0b9830094038de2a41ac
Justin Mullis <justin.mullis@gmail.com>
2016-08-24 08:26:30 -0400

    hardcoded signin, waiting on heroku deployment

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
   
   def home
+    # hard coded this for the moment, waiting on heroku deployment 
     session[:user_id] = 3
     if current_user
       redirect_to customers_path
