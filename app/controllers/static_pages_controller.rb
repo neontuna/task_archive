f67b3ac8af837405fdede0cdee6f264a492ceac8
Justin Mullis <justin.mullis@gmail.com>
2016-08-24 17:08:25 -0400

    remove hard coded user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,6 @@ class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
   
   def home
-    # hard coded this for the moment, waiting on heroku deployment 
-    session[:user_id] = 3
     if current_user
       redirect_to customers_path
     end
